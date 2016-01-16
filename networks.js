07d82a42d59cc26656cbee6be485ed06903eb571
Colin Mackie <cdmackie@gmail.com>
2016-01-15 21:20:59 -0800

    Add data for DOGE and LTC accessed by env

--- networks.js
+++ networks.js
@@ -4,34 +4,91 @@ var hex = function(hex) {
   return new Buffer(hex, 'hex');
 };
 
-exports.livenet = {
-  name: 'livenet',
-  magic: hex('f9beb4d9'),
-  addressVersion: 0x00,
-  privKeyVersion: 128,
-  P2SHVersion: 5,
-  hkeyPublicVersion: 0x0488b21e,
-  hkeyPrivateVersion: 0x0488ade4,
-  genesisBlock: {
-    hash: hex('6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000'),
-    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
-    height: 0,
-    nonce: 2083236893,
-    version: 1,
-    prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1231006505,
-    bits: 486604799,
-  },
-  dnsSeeds: [
-    'seed.bitcoin.sipa.be',
-    'dnsseed.bluematt.me',
-    'dnsseed.bitcoin.dashjr.org',
-    'seed.bitcoinstats.com',
-    'seed.bitnodes.io',
-    'bitseed.xf2.org'
-  ],
-  defaultClientPort: 8333
+var livenet = {
+	BTC:{
+		name: 'livenet',
+		magic: hex('f9beb4d9'),
+		addressVersion: 0x00,
+		privKeyVersion: 128,
+		P2SHVersion: 5,
+		hkeyPublicVersion: 0x0488b21e,
+		hkeyPrivateVersion: 0x0488ade4,
+		genesisBlock: {
+			hash: hex('6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000'),
+			merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
+			height: 0,
+			nonce: 2083236893,
+			version: 1,
+			prev_hash: buffertools.fill(new Buffer(32), 0),
+			timestamp: 1231006505,
+			bits: 486604799,
+		},
+		dnsSeeds: [
+			'seed.bitcoin.sipa.be',
+			'dnsseed.bluematt.me',
+			'dnsseed.bitcoin.dashjr.org',
+			'seed.bitcoinstats.com',
+			'seed.bitnodes.io',
+			'bitseed.xf2.org'
+		],
+		defaultClientPort: 8333
+	},
+	DOGE:{
+		name: 'livenet',
+		magic: hex('c0c0c0c0'),
+		addressVersion: 0x1e,
+		privKeyVersion: 0x9e,
+		P2SHVersion: 5,
+		hkeyPublicVersion: 0x0488b21e,
+		hkeyPrivateVersion: 0x0488ade4,
+		genesisBlock: {
+			hash: hex('9156352c1818b32e90c9e792efd6a11a82fe7956a630f03bbee236cedae3911a'),
+			merkle_root: hex('696ad20e2dd4365c7459b4a4a5af743d5e92c6da3229e6532cd605f6533f2a5b'),
+			height: 0,
+			nonce: 99943,
+			version: 1,
+			prev_hash: buffertools.fill(new Buffer(32), 0),
+			timestamp: 1386325540,
+			bits: 486604799,
+		},
+		dnsSeeds: [
+			'seed.dogecoin.com',
+			'seed.mophides.com',
+			'seed.dglibrary.org',
+			'seed.dogechain.info'
+		],
+		defaultClientPort: 22556
+	},
+	LTC:{
+		name: 'livenet',
+		magic: hex('fbc0b6db'),
+		addressVersion: 0x30,
+		privKeyVersion: 0xb0,
+		P2SHVersion: 5,
+		hkeyPublicVersion: 0x0488c42e,
+		hkeyPrivateVersion: 0x0488e1f4,
+		genesisBlock: {
+			hash: hex('E2BF047E7E5A191AA4EF34D314979DC9986E0F19251EDABA5940FD1FE365A712'),
+			merkle_root: hex('D9CED4ED1130F7B7FAAD9BE25323FFAFA33232A17C3EDF6CFD97BEE6BAFBDD97'),
+			height: 0,
+			nonce: 2084524493,
+			version: 1,
+			prev_hash: buffertools.fill(new Buffer(32), 0),
+			timestamp: 1317972665,
+			bits: 504365040,
+		},
+		dnsSeeds: [
+			'dnsseed.litecointools.com',
+			'dnsseed.litecoinpool.org',
+			'dnsseed.ltc.xurious.com',
+			'dnsseed.koin-project.com',
+			'dnsseed.weminemnc.com'
+		],
+		defaultClientPort: 9333
+	}
 };
+
+exports.livenet = livenet[process.env["BITCORE_COIN"] || "BTC"] || livenet.BTC;
 
 exports.mainnet = exports.livenet;
 
