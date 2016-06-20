0c3a2b415a9779f78a60578ac58706a46fcdb507
Colin Mackie <cdmackie@gmail.com>
2016-06-19 18:56:17 -0700

    Latest

--- networks.js
+++ networks.js
@@ -8,32 +8,32 @@ exports.nets = {
 	BTC:{
 		symbol:'BTC',
 		coinname:'Bitcoin',
-		name: 'livenet',
-		magic: hex('f9beb4d9'),
-		addressVersion: 0x00,
-		privKeyVersion: 128,
-		P2SHVersion: 5,
-		hkeyPublicVersion: 0x0488b21e,
-		hkeyPrivateVersion: 0x0488ade4,
-		genesisBlock: {
-			hash: hex('6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000'),
-			merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
-			height: 0,
-			nonce: 2083236893,
-			version: 1,
-			prev_hash: buffertools.fill(new Buffer(32), 0),
-			timestamp: 1231006505,
-			bits: 486604799,
-		},
-		dnsSeeds: [
-			'seed.bitcoin.sipa.be',
-			'dnsseed.bluematt.me',
-			'dnsseed.bitcoin.dashjr.org',
-			'seed.bitcoinstats.com',
-			'seed.bitnodes.io',
-			'bitseed.xf2.org'
-		],
-		defaultClientPort: 8333
+  name: 'livenet',
+  magic: hex('f9beb4d9'),
+  addressVersion: 0x00,
+  privKeyVersion: 128,
+  P2SHVersion: 5,
+  hkeyPublicVersion: 0x0488b21e,
+  hkeyPrivateVersion: 0x0488ade4,
+  genesisBlock: {
+    hash: hex('6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000'),
+    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
+    height: 0,
+    nonce: 2083236893,
+    version: 1,
+    prev_hash: buffertools.fill(new Buffer(32), 0),
+    timestamp: 1231006505,
+    bits: 486604799,
+  },
+  dnsSeeds: [
+    'seed.bitcoin.sipa.be',
+    'dnsseed.bluematt.me',
+    'dnsseed.bitcoin.dashjr.org',
+    'seed.bitcoinstats.com',
+    'seed.bitnodes.io',
+    'bitseed.xf2.org'
+  ],
+  defaultClientPort: 8333
 	},
 	DOGE:{
 		symbol:'DOGE',
